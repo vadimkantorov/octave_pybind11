2523b25384748617f5b42adfce505841df8bdc6b
Vadim Kantorov <vadimkantorov@gmail.com>
2025-09-18 18:36:29 -0700

    ..

--- octave_pybind11_test.py
+++ octave_pybind11_test.py
@@ -3,7 +3,8 @@ import octave_pybind11
 
 interp = octave_pybind11.OctavePybind11Interpreter()
 
-myarr = np.array([[1.0, 2.0], [3.0, 4.0]], dtype = np.float64)
+# if not column-major, pybind11 will copy-convert to column-major. on C++ side, will memcpy-in a temp Octave matrix object
+myarr = np.array([[1.0, 2.0], [3.0, 4.0]], dtype = np.float64, order = 'F')
 
-print(np.linalg.norm(myarr, "fro"))
-print(interp.norm(myarr))
+print(np.linalg.norm(myarr, "fro")) # 5.477225575051661
+print(interp.norm(myarr))           # 5.477225575051661
